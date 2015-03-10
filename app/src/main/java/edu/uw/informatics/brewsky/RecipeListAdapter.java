906ccbe2ca394f096fac9d540fa9d2be2be87e81
Daniel Nakamura <nakamd@uw.edu>
2015-03-10 14:37:29 -0700

    added the UI for the RecipeListActivity and the RecipeListRow, introduced the drawable folder

--- app/src/main/java/edu/uw/informatics/brewsky/RecipeListAdapter.java
+++ app/src/main/java/edu/uw/informatics/brewsky/RecipeListAdapter.java
@@ -2,10 +2,12 @@ package edu.uw.informatics.brewsky;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,6 +22,10 @@ public class RecipeListAdapter extends ArrayAdapter<Recipe> {
 
     static class RecipeListHolder {
         TextView name;
+        TextView abv;
+        TextView brewTime;
+        ImageView type;
+        ImageView rating;
     }
 
     public RecipeListAdapter(Context context, int resource, ArrayList<Recipe> data) {
@@ -41,15 +47,32 @@ public class RecipeListAdapter extends ArrayAdapter<Recipe> {
 
             holder = new RecipeListHolder();
             // Set up view
+            holder.name = (TextView)row.findViewById(R.id.recipe_list_name);
+            holder.abv = (TextView)row.findViewById(R.id.recipe_list_abv);
+            holder.brewTime = (TextView)row.findViewById(R.id.recipe_list_brew_time);
+            holder.rating = (ImageView)row.findViewById(R.id.recipe_list_rating);
+            holder.type = (ImageView)row.findViewById(R.id.recipe_list_type);
 
             row.setTag(holder);
         } else {
             holder = (RecipeListHolder)row.getTag();
         }
         Recipe recipe = data.get(position);
-
         //Set holder variables
+        Log.i("RecipeListActivity", "Need getters from recipe");
+        holder.name.setText("Testing");
+        holder.abv.setText("8.9%");
+        holder.brewTime.setText("1 month");
+        holder.rating.setImageResource(R.drawable.threeStar);
+        holder.type.setImageResource(R.drawable.amberAle);
 
+        /*
+        holder.name.setText(recipe.getName());
+        holder.abv.setText(recipe.getAbv());
+        holder.brewTime.setText(recipe.getBrewTime());
+        holder.rating.setText(recipe.getRating());
+        holder.type.setText(recipe.getType());
+*/
 
 
         return row;
